ea4f306f58794a3cfc2db1a51794f1f8feb5fb86
Andries Niemandt <andries.niemandt91@gmail.com>
2024-07-09 20:43:02 +0200

    basic getter using tokio axum

--- README.md
+++ README.md
@@ -1,8 +1,22 @@
 # Rust Axum
 
+## Install required dependencies
+
 ```bash
 cargo add tokio --features full && \
 cargo add serde@1.0.195 --features derive && \
 cargo add chrono@0.4.34 --features serde && \
 cargo add axum@0.7.5 jsonwebtoken@9.3.0 bcrypt@0.15.1 serde_json@1.0.95
+```
+
+## Compile the project
+
+```bash
+cargo build
+```
+
+## Run the binary
+
+```bash
+./target/debug/rust-axum
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use axum;
+use axum::{Router, routing::get};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    let srv = TcpListener::bind("127.0.0.1:8080")
+        .await
+        .expect("Unable to connect to the server");
+
+    let routes = Router::new()
+        .route("/", get(|| async { "Hello from Rust-Axum" }));
+
+    axum::serve(srv, routes)
+        .await
+        .expect("Error serving application");
 }
